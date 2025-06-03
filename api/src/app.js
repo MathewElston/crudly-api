42269927ed8d7e2937fa0a40b8b3b691a487ee8d
Mathew Elston <elstonmathew@yahoo.com>
2025-06-02 19:16:34 -0700

    wrote most of the getRecord function. Need to implmenet a schema enforcer

--- api/src/app.js
+++ api/src/app.js
@@ -2,30 +2,51 @@ import express from "express";
 
 import db from "./lib/database/db.js";
 
+import RecordService from "./lib/services/RecordService.js";
+
 const testUser = {
   userId: 1,
   projectId: 2,
-  projectName: "Test Project",
 };
+
+const recordService = new RecordService(db);
 
 const app = express();
 
 app.get("/:project/:table", async (req, res) => {
   const { project, table } = req.params;
-  try {
+  /*   try {
     const [results, fields] = await db.execute(
       `SELECT records->'$.${table}' as result FROM User_Projects WHERE project_name = ?`,
       [project]
     );
     const records = results[0].result;
-    console.log(results);
-    console.log(records);
     res.json(records);
   } catch (error) {
     console.error(error.message);
     res
       .status(500)
       .json({ message: "There was an internal error on the server." });
+  } */
+});
+
+app.get("/:project", async (req, res) => {
+  const { project } = req.params;
+
+  try {
+    const results = await recordService.getAllProjectRecords(
+      testUser.userId,
+      project
+    );
+    res.json(results);
+  } catch (error) {
+    console.error(error.status);
+    if (error.status === 404) {
+      return res.status(404), json({ message: error.message });
+    }
+    return res.status(500).json({
+      message: error.message,
+    });
   }
 });
 
---
+++ api/src/lib/services/RecordService.js
@@ -0,0 +1,52 @@
+class RecordService {
+  constructor(db) {
+    this.db = db;
+  }
+  async getAllProjectRecords(userId, projectName) {
+    const [results] = await this.db.execute(
+      `SELECT records AS result FROM User_Projects  WHERE user_id = ? AND project_name = ?`,
+      [userId, projectName]
+    );
+
+    if (results.length === 0) {
+      throw new Error("No records found.");
+    }
+
+    const records = results[0].result;
+
+    return records;
+  }
+
+  async getTableRecords(userId, projectName, tableName) {
+    const [results] = await this.db.execute(
+      `SELECT records->'$.${tableName}' AS result FROM User_Projects WHERE user_id = ? AND project_name = ?`,
+      [userId, projectName]
+    );
+
+    if (results.length === 0) {
+      throw new Error("No records found");
+    }
+
+    const records = results[0].result;
+
+    return records;
+  }
+  async getRecord(userId, projectName, tableName, recordId) {
+    const [results] = await this.db.execute(`
+        select orders.*
+        from User_Projects,
+        JSON_TABLE(
+        records->'$.${tableName}',
+        '$[*]' COLUMNS (
+        id INT PATH '$.id',
+        userId INT PATH '$.userId',
+        item VARCHAR(100) PATH '$.item',
+        price DOUBLE PATH '$.price',
+        quantity INT PATH '$.quantity'
+        )
+    ) 
+    AS orders
+    WHERE orders.id = ${recordId}`);
+  }
+}
+export default RecordService;
