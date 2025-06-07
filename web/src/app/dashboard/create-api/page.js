3af2720f415adc7fda58747dc7ab7adf4124840c
Mathew Elston <elstonmathew@yahoo.com>
2025-06-07 16:44:44 -0700

    started create-api workflow. Need to fix FileUploader and implmenet selections for premade databases

--- web/src/app/dashboard/create-api/page.js
+++ web/src/app/dashboard/create-api/page.js
@@ -1,7 +1,54 @@
+"use client";
+import FileUpload from "@/components/FileUpload";
+import HoverCard from "@/components/HoverCard";
+import { Box, Stack, Typography, Button } from "@mui/material";
+import { useState } from "react";
+
 export default function CreateApiPage() {
-
-    return (
-
-    );
-    
-};
+  const cardConfig = {
+    width: "50%",
+    height: 250,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const [apiSelect, setApiSelect] = useState(null);
+  return (
+    <Stack sx={{ backgroundColor: "#121212", minHeight: "100vh", p: 4 }}>
+      <Typography sx={{ p: 4 }}>Select a Database type</Typography>
+      <Stack sx={{ width: "50%" }} spacing={4} direction={"row"}>
+        <HoverCard
+          name="Premade Card"
+          onClick={() => setApiSelect(1)}
+          sx={{
+            ...cardConfig,
+          }}
+        >
+          <Typography sx={{}}>Premade</Typography>
+        </HoverCard>
+        <HoverCard
+          name="Custom Card"
+          sx={{
+            ...cardConfig,
+          }}
+          onClick={() => setApiSelect(2)}
+        >
+          <Typography sx={{}}>Custom</Typography>
+        </HoverCard>
+      </Stack>
+      <Stack
+        spacing={4}
+        sx={{
+          width: "50%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {apiSelect === 1 && <p>Generate Premade section</p>}
+        {apiSelect === 2 && <FileUpload></FileUpload>}
+        {apiSelect && <Button variant="contained">Create API </Button>}
+      </Stack>
+    </Stack>
+  );
+}
--- web/src/components/HoverCard.js
+++ web/src/components/HoverCard.js
@@ -1,20 +1,30 @@
+"use client";
 import { Paper } from "@mui/material";
 
-export default function HoverCard({ cardWidth = "40%", children }) {
+export default function HoverCard({
+  cardWidth = "40%",
+  onClick,
+  children,
+  sx = {},
+  ...props
+}) {
   return (
     <Paper
-      elevation={3}
+      elevation={2}
       sx={{
+        width: cardWidth,
         p: 2,
-        width: { cardWidth },
         cursor: "pointer",
         transition: "border 0.2s ease",
         border: "1px solid transparent",
         "&:hover": {
           border: "1px solid",
-          borderColor: "tertiary.main", // or any custom color
+          borderColor: "tertiary.main",
         },
+        ...sx,
       }}
+      onClick={onClick}
+      {...props}
     >
       {children}
     </Paper>
