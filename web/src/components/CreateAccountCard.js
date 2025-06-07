2bebac42d7da9f69ea449867cd59e2b126aba4d2
Mathew Elston <elstonmathew@yahoo.com>
2025-06-06 19:55:01 -0700

    fixed bug for topend

--- web/src/components/CreateAccountCard.js
+++ web/src/components/CreateAccountCard.js
@@ -5,14 +5,12 @@ import { TextField, Box, Typography, Link } from "@mui/material";
 
 export default function CreateAccountCard() {
   return (
-    <Paper sx={{ height: 400, width: "20%" }}>
+    <Paper sx={{ height: 650, width: "20%" }}>
       <Box
         sx={{
           height: "100%",
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
-          px: 4,
         }}
       >
         <Stack spacing={3}>
