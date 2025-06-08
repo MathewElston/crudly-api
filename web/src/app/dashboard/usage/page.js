9cb43a893f20815e1a77768530827733f98d5d71
Mathew Elston <elstonmathew@yahoo.com>
2025-06-07 22:10:14 -0700

    centered UsagePage

--- web/src/app/dashboard/usage/page.js
+++ web/src/app/dashboard/usage/page.js
@@ -6,16 +6,16 @@ import { Typography, Stack, Box } from "@mui/material";
 
 export default function UsagePage() {
   return (
-    <>
-      <Stack spacing={2} padding={2}>
-        <Typography variant={"h3"} component={"h2"}>
-          API Usage
-        </Typography>
-        <Box>
-          <CircleProgress value={75} max={100} />
-        </Box>
+    <Stack spacing={2} padding={2} alignItems={"center"}>
+      <Typography variant={"h3"} component={"h2"}>
+        API Usage
+      </Typography>
+      <Box>
+        <CircleProgress value={75} max={100} />
+      </Box>
+      <Box sx={{ width: 600, height: 300 }}>
         <SimpleLineChart strokeColor={theme.palette.primary.dark} />
-      </Stack>
-    </>
+      </Box>
+    </Stack>
   );
 }
