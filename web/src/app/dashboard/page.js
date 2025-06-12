5d389f9e31ae7e71dc9cbc23478ee25af8d6df10
Mathew Elston <elstonmathew@yahoo.com>
2025-06-11 23:57:07 -0700

    implmented reset api key

--- web/src/app/dashboard/page.js
+++ web/src/app/dashboard/page.js
@@ -6,23 +6,28 @@ import { getApiKey } from "@/server/api/apiServerActions";
 import ApiManager from "@/components/ApiManager";
 
 export default async function UsagePage() {
-  const user = await getUser();
-  const { apiKey } = await getApiKey(user.id);
+  try {
+    const user = await getUser();
+    const { apiKey } = await getApiKey(user.id);
 
-  const data = [
-    { name: "Jan", value: 400 },
-    { name: "Feb", value: 300 },
-    { name: "Mar", value: 200 },
-    { name: "Apr", value: 278 },
-    { name: "May", value: 189 },
-  ];
-  return (
-    <Stack spacing={2} padding={2} alignItems={"center"}>
-      <Stack direction={"row"} spacing={10}>
-        <Typography variant="h1">Hi, {user.username}</Typography>
-        <ApiManager userId={user.id} apiKey={apiKey} />
+    const data = [
+      { name: "Jan", value: 400 },
+      { name: "Feb", value: 300 },
+      { name: "Mar", value: 200 },
+      { name: "Apr", value: 278 },
+      { name: "May", value: 189 },
+    ];
+    return (
+      <Stack spacing={2} padding={2} alignItems={"center"}>
+        <Stack direction={"row"} spacing={10}>
+          <Typography variant="h1">Hi, {user.username}</Typography>
+          <ApiManager userId={user.id} userKey={apiKey} />
+        </Stack>
+        <UsageGraph data={data} currentProgress={40} maxProgress={1000} />
       </Stack>
-      <UsageGraph data={data} currentProgress={40} maxProgress={1000} />
-    </Stack>
-  );
+    );
+  } catch (error) {
+    console.error("Page error:", error);
+    return <div>Error loading page: {error.message}</div>;
+  }
 }
--- web/src/components/ApiManager.js
+++ web/src/components/ApiManager.js
@@ -2,15 +2,27 @@
 import SecretText from "@/components/SecretText";
 import { Typography, Stack, Button } from "@mui/material";
 import { resetApiKey } from "@/server/api/apiServerActions";
+import { useState } from "react";
 
-export default async function ApiManager({ userId, apiKey }) {
+export default function ApiManager({ userId, userKey }) {
+  const [keyState, setKeyState] = useState(userKey);
   async function handleClick() {
     const { success, apiKey } = await resetApiKey(userId);
+    if (success) {
+      setKeyState(apiKey);
+    }
   }
   return (
-    <Stack direction={"row"} spacing={10}>
-      <SecretText label={"API Key"} secretText={apiKey} />
-      <Button variant={"contained"}>Reset Key</Button>
+    <Stack spacing={2}>
+      <SecretText label={"API Key"} secretText={keyState} />
+      <Button
+        onClick={handleClick}
+        color="tertiary"
+        sx={{ width: "50%" }}
+        variant={"contained"}
+      >
+        Reset Key
+      </Button>
     </Stack>
   );
 }
