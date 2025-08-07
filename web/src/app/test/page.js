affa77a37c28195890003fd416fc26c2548e1a59
Mathew Elston <elstonmathew@yahoo.com>
2025-08-06 19:03:16 -0700

    started selectionBox component for selecting projects, and other items as needed

--- web/src/app/test/page.js
+++ web/src/app/test/page.js
@@ -1,11 +1,17 @@
-import TestCard from "@/components/TestCard";
+import SelectionBox from "@/components/SelectionBox";
+
+import { Box } from "@mui/material";
 
 import { getUser } from "@/server/data-access-layer/getUser";
-
 import { createApiKey } from "@/server/api/apiServerActions";
 
 export default async function TestPage() {
+  const menuItems = ["hi", "mom", "ilove you"];
   const user = await getUser();
 
-  return <TestCard name={user.username} userId={user.id} />;
+  return (
+    <Box sx={{ mt: 5, width: 200, height: 200 }}>
+      <SelectionBox inputLabel={"Project"} menuItems={menuItems}></SelectionBox>
+    </Box>
+  );
 }
